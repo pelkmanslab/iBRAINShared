9e14c84ed2660c2fe1da9191490951611645d1c6
Owen Feehan <owenfeehan@users.noreply.github.com>
2015-04-28 17:22:45 +0200

    updated documentation

--- Scripts/buildDocumentation.m
+++ Scripts/buildDocumentation.m
@@ -7,12 +7,8 @@
 
 % PARAMETERS START
 
-disp(pwd());
-
 % TAP file to output
 docDirRelToTopLevel = 'docHTML/';
-
-disp(pwd());
 
 % PARAMETERS END
 
@@ -22,14 +18,14 @@ if (strcmp(deepestFolder,'Scripts')==0)
    error('Not in Scripts/ directory');
 end
 
-addpath('thirdPartyTools/m2html/')
+addpath( fullfile(pwd(),'thirdPartyTools/m2html') );
 
 % Gets the top-level folder name
 [upperPath, deepestFolderTopLevel, ignoreThisStr] = fileparts(upperPath);
 
-%cd('../..');
+cd('../..');
 
 % This needs to be run from one-folder higher up than the current directory
 %  e.g.  "." in top-level  does not work
 %       but "iBRAINShared" in top-level/.. does
-%m2html('mfiles',deepestFolderTopLevel,'recursive','on','htmlDir', fullfile(deepestFolderTopLevel,docDirRelToTopLevel))
+m2html('mfiles',deepestFolderTopLevel,'recursive','on','htmlDir', fullfile(deepestFolderTopLevel,docDirRelToTopLevel))
