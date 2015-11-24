90393ccf053b3187fcd05f40b72e5b6638066d84
Thomas Stoeger <thomas.stoeger@imls.uzh.ch>
2015-11-24 18:52:35 +0100

    added illumination correction to heuristics

--- iBRAIN/CreateIllcorJpgsOverview/create_jpgs_illumination_corrected.m
+++ iBRAIN/CreateIllcorJpgsOverview/create_jpgs_illumination_corrected.m
@@ -172,7 +172,7 @@ function create_jpgs_illumination_corrected(strTiffPath, strOutputPath, strSearc
     matChannelIntensities = NaN(max(matChannelsPresent),2);
     
     % sample a test image outside the parfor loop
-    tempImage = imread(fullfile(strTiffPath,cellFileList{1}));
+    ReferenceImage = imread(fullfile(strTiffPath,cellFileList{1}));
     
     for iChannel = matChannelsPresent
         
@@ -192,12 +192,7 @@ function create_jpgs_illumination_corrected(strTiffPath, strOutputPath, strSearc
         % contains lower and upper quantile intensity per image
         matQuantiles= NaN(intNumOfSamplesPerChannel,2);
         
-%         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%         %%%% WE COULD ADD ILLUMINATION CORRECTION HERE %%%% 
-%         % for illumination correction
-%         matMeanImage = [];
-%         matStdevImage = [];
-%         %%%%
+
         
 % % %         fprintf('%s: \tprogress 0%%',mfilename)
         for i = 1:intNumOfSamplesPerChannel; 
@@ -208,14 +203,18 @@ function create_jpgs_illumination_corrected(strTiffPath, strOutputPath, strSearc
 % % %                 fprintf(' %d%%',round(100*(i/intNumOfSamplesPerChannel)))
 % % %             end
             
-            try
-                tempImage = imread(fullfile(strTiffPath,strImageName)); %#ok<PFTIN,PFTUS>
-            catch %#ok<CTCH>
-                warning('matlab:bsBla','%s:  failed to load image %s',mfilename,strImageName)
-            end
-
-            % get average lower and upper 5% quantiles per sampled image
-            matQuantiles(i,:) = quantile(single(tempImage(:)),matQuantileSettings)';
+strImageForHeuristics = fullfile(strTiffPath,strImageName);
+        % [TS151024] only conceptual difference to original create_jpg:
+        % shrinking of images. Otherwise it would be slow, when illumination
+        % correction is applied. During last years, a scaling
+        % factor of 20 has been working very robustly without loss
+        % of quality, in a similarscript/function, that obtains
+        % parameters for spot detection, and also gets extrema
+        ShrinkFactorForHeuristics = 20;
+        ImageForHeuristics = imread_shrunken_illumination_corrected(strImageForHeuristics, ShrinkFactorForHeuristics);
+        
+                    % get average lower and upper 5% quantiles per sampled image
+        matQuantiles(i,:) = quantile(single(ImageForHeuristics(:)),matQuantileSettings)';
 
         end
         fprintf(' done\n')
@@ -230,12 +229,12 @@ function create_jpgs_illumination_corrected(strTiffPath, strOutputPath, strSearc
     %%% START MERGE AND STITCH AND JPG CONVERSION %%%
     
     % calculate shrink factor dynamically to approach target jpg dimensions
-    intShrinkFactor = floor(max((size(tempImage).*matStitchDimensions) ./ matTargetImageSize));
+    intShrinkFactor = floor(max((size(ReferenceImage).*matStitchDimensions) ./ matTargetImageSize));
     if intShrinkFactor < 2; intShrinkFactor = 2; end
     fprintf('%s: dynamically determined shrinkfactor to be %d.\n',mfilename,intShrinkFactor);
     
-    %matImageSize = round(size(tempImage)/intShrinkFactor);
-    matImageSize = size(imresize(tempImage,1/intShrinkFactor));
+    %matImageSize = round(size(ReferenceImage)/intShrinkFactor);
+    matImageSize = size(imresize(ReferenceImage,1/intShrinkFactor));
     
     if length(matChannelsPresent) == 4
         matChannelOrder = [3,2,1,0; ... % BLUE, GREEN, RED, nothing
